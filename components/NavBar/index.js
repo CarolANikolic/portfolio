f55d27f916a2e3361e9c7589b3cf37a795273a74
Carol A. Nikolic <carolnikolic@gmail.com>
2024-08-24 21:50:31 +0200

    feat: handle link click by closing the dropdown menu and activating link styles.

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -8,6 +8,13 @@ import { useState } from "react";
 export default function NavBar() {
     const [menu, setMenu] = useState(false);
 
+    const [activeItem, setActiveItem] = useState("");
+
+    const handleClick = (id) => {
+        setActiveItem(id);
+        setMenu(false);
+    };
+
     return (
         <header className={styles.header}>
             <div>
@@ -21,16 +28,50 @@ export default function NavBar() {
             <nav>
                 <ul className={`${styles.navList} ${menu && styles.showMenu}`}>
                     <li>
-                        <Link href="#about" className={styles.navListItem}>About</Link>
+                        <Link 
+                            href="#about" 
+                            className={`${styles.navListItem} 
+                                        ${activeItem === "about" ? 
+                                        styles.active : 
+                                        ""}`}
+                            onClick={() => handleClick("about")}
+                        >
+                            About
+                        </Link> 
                     </li>
                     <li>
-                        <Link href="#projects" className={styles.navListItem}>Projects</Link>
+                        <Link 
+                            href="#projects" 
+                            className={`${styles.navListItem} ${activeItem === "projects" 
+                                        ? styles.active : 
+                                        ""}`}
+                            onClick={() => handleClick("projects")}
+                        >
+                            Projects
+                        </Link>
                     </li>
                     <li>
-                        <Link href="#skills" className={styles.navListItem}>Skills</Link>
+                        <Link 
+                            href="#skills" 
+                            className={`${styles.navListItem} ${
+                                        activeItem === "skills" ? 
+                                        styles.active : 
+                                        ""}`}
+                            onClick={() => handleClick("skills")}
+                        >
+                            Skills
+                        </Link>
                     </li>
                     <li>
-                        <Link href="#contact" className={styles.navListItem}>Contact</Link>
+                        <Link 
+                            href="#contact" 
+                            className={`${styles.navListItem} ${activeItem === "contact" ? 
+                                        styles.active : 
+                                        ""}`}
+                            onClick={() => handleClick("contact")}
+                        >
+                            Contact
+                        </Link>
                     </li>
                 </ul>
             </nav>
