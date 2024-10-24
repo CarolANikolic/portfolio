7b5879342b67727a4c0e029abec467510b0260fe
Carol A. Nikolic <carolnikolic@gmail.com>
2024-10-24 18:20:34 +0200

    feat: add portolio work section title and subtitle.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,33 @@
 import AboutMe from "@/components/AboutMe";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+import TitleAndText from "@/components/TitleAndText";
 
 export default function Home() {
   return (
     <main>
       <AboutMe/>
+
+      <section className={styles.portfolioSection}>
+        <div className={styles.portfolioContainer}>
+          <TitleAndText
+          title="h2"
+          titleContent="Recent Projects & Work"
+          text=""
+          alignment="center"
+          color="boldBlack"
+          fontSize="medium"
+          />
+
+          <TitleAndText
+          title=""
+          text="Here are a few of the projects I’ve been working lately."
+          alignment="left"
+          color="regularBlack"
+          fontSize="small"
+          />
+        </div> 
+      </section>
+
     </main>
   );
 }
