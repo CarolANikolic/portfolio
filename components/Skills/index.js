18ca8062adc6ec0a5ea1b0d82f7f552fc7d23bb3
Carol A. Nikolic <carolnikolic@gmail.com>
2025-01-24 15:08:31 -0300

    styles: change background color of skills section to yellow.

--- components/Skills/index.js
+++ components/Skills/index.js
@@ -5,26 +5,28 @@ import styles from "./index.module.css";
 export default function Skills() {
     return (
         <section className={styles.skillsSection}>
-            <div>
-                <TitleAndText
-                title="h2"
-                titleContent="Check Out Some of My Skills"
-                alignment="center"
-                color="boldBlack"
-                fontSize="big"
-                />
-            </div>
-            
-            <div className={styles.skillsGrid}>
-                {skills.map((skill) => (
+            <div className={styles.skillsSectionContainer}>
+                <div>
                     <TitleAndText
-                        key={skill}
-                        text={skill}
-                        alignment="center"
-                        color="regularBlack"
-                        fontSize="medium"
+                    title="h2"
+                    titleContent="Check Out Some of My Skills"
+                    alignment="center"
+                    color="boldBlack"
+                    fontSize="big"
                     />
-                ))}
+                </div>
+                
+                <div className={styles.skillsGrid}>
+                    {skills.map((skill) => (
+                        <TitleAndText
+                            key={skill}
+                            text={skill}
+                            alignment="center"
+                            color="regularBlack"
+                            fontSize="medium"
+                        />
+                    ))}
+                </div>
             </div>
         </section>
     );
